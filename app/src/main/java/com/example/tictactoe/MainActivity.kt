9b826b88212d006b72710d853b10a2824a76b3b7
Your Name <tamuna.kakhidze.1@btu.edu.ge>
2020-12-04 19:53:30 +0400

    init

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -0,0 +1,271 @@
+package com.example.tictactoe
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlin.math.round
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var button1: Button
+    private lateinit var button2: Button
+    private lateinit var button3: Button
+    private lateinit var button4: Button
+    private lateinit var button5: Button
+    private lateinit var button6: Button
+    private lateinit var button7: Button
+    private lateinit var button8: Button
+    private lateinit var button9: Button
+
+    private lateinit var resetButton: Button
+
+    private var firstPlayer = ArrayList<Int>()
+    private var secondPlayer = ArrayList<Int>()
+    var roundCount = 0
+
+
+    private var activePlayer = 1
+    fun resetButton(view: View) {
+        roundCount = 0
+        button1.setBackgroundResource(android.R.drawable.btn_default)
+        button2.setBackgroundResource(android.R.drawable.btn_default)
+        button3.setBackgroundResource(android.R.drawable.btn_default)
+        button4.setBackgroundResource(android.R.drawable.btn_default)
+        button5.setBackgroundResource(android.R.drawable.btn_default)
+        button6.setBackgroundResource(android.R.drawable.btn_default)
+        button7.setBackgroundResource(android.R.drawable.btn_default)
+        button8.setBackgroundResource(android.R.drawable.btn_default)
+        button9.setBackgroundResource(android.R.drawable.btn_default)
+
+        button1.text = ""
+        button2.text = ""
+        button3.text = ""
+        button4.text = ""
+        button5.text = ""
+        button6.text = ""
+        button7.text = ""
+        button8.text = ""
+        button9.text = ""
+
+        firstPlayer.clear()
+        secondPlayer.clear()
+        activePlayer = 1
+
+        button1.isEnabled = true
+        button2.isEnabled = true
+        button3.isEnabled = true
+        button4.isEnabled = true
+        button5.isEnabled = true
+        button6.isEnabled = true
+        button7.isEnabled = true
+        button8.isEnabled = true
+        button9.isEnabled = true
+        fun color (clickedView : Button) {
+
+        if (clickedView is Button) {
+
+            clickedView.setBackgroundColor(Color.GRAY)
+            clickedView.setBackgroundColor(android.R.drawable.btn_default)
+        }
+    }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        init()
+
+    }
+
+    private fun init() {
+
+        button1 = findViewById(R.id.button1)
+        button2 = findViewById(R.id.button2)
+        button3 = findViewById(R.id.button3)
+        button4 = findViewById(R.id.button4)
+        button5 = findViewById(R.id.button5)
+        button6 = findViewById(R.id.button6)
+        button7 = findViewById(R.id.button7)
+        button8 = findViewById(R.id.button8)
+        button9 = findViewById(R.id.button9)
+
+        resetButton = findViewById(R.id.resetButton)
+
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+        button5.setOnClickListener(this)
+        button6.setOnClickListener(this)
+        button7.setOnClickListener(this)
+        button8.setOnClickListener(this)
+        button9.setOnClickListener(this)
+
+    }
+
+    override fun onClick(clickedView: View?) {
+
+        if (clickedView is Button) {
+
+            var buttonNumber = 0
+
+            when (clickedView.id) {
+                R.id.button1 -> buttonNumber = 1
+                R.id.button2 -> buttonNumber = 2
+                R.id.button3 -> buttonNumber = 3
+                R.id.button4 -> buttonNumber = 4
+                R.id.button5 -> buttonNumber = 5
+                R.id.button6 -> buttonNumber = 6
+                R.id.button7 -> buttonNumber = 7
+                R.id.button8 -> buttonNumber = 8
+                R.id.button9 -> buttonNumber = 9
+            }
+
+            if (buttonNumber != 0) {
+                playGame(clickedView, buttonNumber)
+            }
+
+        }
+
+
+    }
+
+    private fun playGame(clickedView: Button, buttonNumber: Int) {
+        if (activePlayer == 1) {
+            clickedView.text = "X"
+            clickedView.setBackgroundColor(Color.RED)
+            firstPlayer.add(buttonNumber)
+            roundCount += 1
+            activePlayer = 2
+        } else {
+            clickedView.text = "O"
+            clickedView.setBackgroundColor(Color.YELLOW)
+            secondPlayer.add(buttonNumber)
+            roundCount += 1
+            activePlayer = 1
+        }
+        clickedView.isEnabled = false
+        check()
+    }
+
+    private fun check() {
+
+        var winnerPlayer = 0
+
+        if (firstPlayer.contains(1) && firstPlayer.contains(2) && firstPlayer.contains(3)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(1) && secondPlayer.contains(2) && secondPlayer.contains(3)) {
+            winnerPlayer = 2
+        }
+
+        if (firstPlayer.contains(4) && firstPlayer.contains(5) && firstPlayer.contains(6)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(4) && secondPlayer.contains(5) && secondPlayer.contains(6)) {
+            winnerPlayer = 2
+        }
+
+        if (firstPlayer.contains(7) && firstPlayer.contains(8) && firstPlayer.contains(9)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(7) && secondPlayer.contains(8) && secondPlayer.contains(9)) {
+            winnerPlayer = 2
+        }
+
+        if (firstPlayer.contains(1) && firstPlayer.contains(4) && firstPlayer.contains(7)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(1) && secondPlayer.contains(4) && secondPlayer.contains(7)) {
+            winnerPlayer = 2
+        }
+
+        if (firstPlayer.contains(2) && firstPlayer.contains(5) && firstPlayer.contains(8)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(2) && secondPlayer.contains(5) && secondPlayer.contains(8)) {
+            winnerPlayer = 2
+        }
+
+        if (firstPlayer.contains(3) && firstPlayer.contains(6) && firstPlayer.contains(9)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(3) && secondPlayer.contains(6) && secondPlayer.contains(9)) {
+            winnerPlayer = 2
+        }
+
+        if (firstPlayer.contains(3) && firstPlayer.contains(5) && firstPlayer.contains(7)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(3) && secondPlayer.contains(5) && secondPlayer.contains(7)) {
+            winnerPlayer = 2
+        }
+
+        if (firstPlayer.contains(1) && firstPlayer.contains(5) && firstPlayer.contains(9)) {
+            winnerPlayer = 1
+        }
+        if (secondPlayer.contains(1) && secondPlayer.contains(5) && secondPlayer.contains(9)) {
+            winnerPlayer = 2
+        }
+
+
+
+            if (winnerPlayer != 0) {
+                if (winnerPlayer == 1) {
+                    Toast.makeText(this, "X wins!", Toast.LENGTH_LONG).show()
+                    disable_Buttons()
+                } else {
+                    Toast.makeText(this, "O wins!", Toast.LENGTH_LONG).show()
+                    disable_Buttons()
+                }
+
+
+            }
+            if (roundCount == 9 && winnerPlayer != 1 && winnerPlayer != 2) {
+                Toast.makeText(this, "TIE - No winner", Toast.LENGTH_LONG).show()
+                disable_Buttons()
+
+            }
+
+        }
+        fun disable_Buttons() {
+            button1.isEnabled = false
+            button2.isEnabled = false
+            button3.isEnabled = false
+            button4.isEnabled = false
+            button5.isEnabled = false
+            button6.isEnabled = false
+            button7.isEnabled = false
+            button8.isEnabled = false
+            button9.isEnabled = false
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
